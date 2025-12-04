43e42b02a07ae7219b02d56e0560b0f1b9dd9230
Kogetsuki <rozenbastien@gmail.com>
2025-12-04 19:08:36 +0100

    Test not skipping deployment

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.static('dist'))
 
 app.get('/version', (req, res) =>
-  res.send('VERSIONING'))
+  res.send('VERSIONING WITH BUMP TAG #SKIP'))
 
 app.get('/health', (req, res) => {
   res.send('ok')
