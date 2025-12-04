51f39af57274f7640b74bf562b86879c1b95a872
Kogetsuki <rozenbastien@gmail.com>
2025-12-04 16:15:15 +0100

    health check

--- app.js
+++ app.js
@@ -9,6 +9,10 @@ app.use(express.static('dist'))
 app.get('/version', (req, res) =>
   res.send('1'))
 
+app.get('/health', (req, res) => {
+  res.send('ok')
+})
+
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`server started on port ${PORT}`)
